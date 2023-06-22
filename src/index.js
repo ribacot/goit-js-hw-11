5b0fbae81c5825fa1122ffc9dc62086e90b6fba4
ribacot <remontdiskov1979@gmail.com>
2023-06-22 09:38:26 +0300

    search_error404?

--- src/index.js
+++ src/index.js
@@ -2,19 +2,15 @@ import axios from 'axios';
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Loading } from 'notiflix';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// let lightbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-//   overlayOpacity: 0.9,
-//   closeText: '&#10007;',
-// });
-
-// lightbox.on('error.simplelightbox', function (e) {
-//   console.log(e); // some usefull information
-// });
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.9,
+  closeText: '&#10007;',
+});
 
 const formEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.js-gallery');
@@ -27,7 +23,7 @@ loadMoreEl.addEventListener('click', onLoad);
 const options = {
   q: '',
   page: null,
-  per_page: 10,
+  per_page: 40,
 };
 
 async function onSearch(e) {
@@ -45,56 +41,69 @@ async function onSearch(e) {
     await createPageGallery(options);
     e.target.elements.searchQuery.value = '';
   } catch {
-    Notify.failure('errrr');
+    // console.log('перед errrr')
+    err => Notify.failure(err.message);
+    loadMoreEl.classList.add('visually-hidden');
+
     e.target.elements.searchQuery.value = '';
   } finally {
     Loading.remove();
+    lightbox.refresh();
   }
 }
 
-function onLoad() {
+async function onLoad() {
   options.page += 1;
   createPageGallery(options).catch(err => console.log(err.message));
 }
 
 async function createPageGallery(optionsObj) {
-      loadMoreEl.classList.remove('visually-hidden');
+  loadMoreEl.classList.remove('visually-hidden');
 
   const { page, per_page } = optionsObj;
   const searhObj = await serviceImage(optionsObj);
-
-  console.log('searhObj:', searhObj);
-  console.log(page);
-  console.log(searhObj.totalHits / per_page);
-  if (page - searhObj.totalHits / per_page >= 0) {
-    loadMoreEl.classList.add('visually-hidden');
-    Notify.info(
-      "We're sorry, but you've reached the end of search results."
-    );
+  if (page === 1) {
+    Notify.info(`Hooray! We found ${searhObj.totalHits} images`);
   }
-  galleryEl.insertAdjacentHTML('beforeend', createGalerryDom(searhObj));
-  Loading.remove(300);
-}
 
-async function serviceImage(optionsObj) {
-  Loading.circle();
-  const BASE_URL = 'https://pixabay.com/api/';
-  const resp = await axios.get(BASE_URL, {
-    params: {
-      ...optionsObj,
-      orientation: 'horizontal',
-      mage_type: 'photo',
-      safesearch: true,
-      key: '37410571-78e708f3fcce6ce73b7e36a87',
-    },
-  });
-  if (!resp.data.total) {
+  if (searhObj.hits.length === 0) {
     loadMoreEl.classList.add('visually-hidden');
-    return Notify.failure(
+    return Notify.info(
       'Sorry, there are no images matching your search query. Please try again.'
     );
   }
-  return resp.data;
+  // page - searhObj.totalHits / per_page > 0
+  if (page >= searhObj.totalHits / per_page) {
+    loadMoreEl.classList.add('visually-hidden');
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
+  galleryEl.insertAdjacentHTML('beforeend', createGalerryDom(searhObj));
+  Loading.remove(300);
+  lightbox.refresh();
+}
+
+async function serviceImage(optionsObj) {
+  try {
+    Loading.circle();
+    const BASE_URL = 'https://pixabay.com/api/';
+    const resp = await axios.get(BASE_URL, {
+      params: {
+        ...optionsObj,
+        orientation: 'horizontal',
+        mage_type: 'photo',
+        safesearch: true,
+        key: '37410571-78e708f3fcce6ce73b7e36a87',
+      },
+    });
+    console.log(resp)
+    console.log(resp.data.total);
+    console.log(options.per_page);
+
+    return resp.data;
+  } catch {
+    console.log(resp.statusText)
+    // err=>Notify.failure(err)
+  }
 }
 
 function createGalerryDom(obj) {
@@ -109,8 +118,8 @@ function createGalerryDom(obj) {
         comments,
         downloads,
         views,
-      }) => `<div class="photo-card picture "><div class="gallery__item gallery"><a href="${largeImageURL}"> <img  class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" width="300" />
- </a></div>
+      }) => `<a href="${largeImageURL}"> <div class="photo-card picture "><div class="gallery__item "><img  class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" width="300" />
+</div>
  
   <div class="info">
     <p class="info-item">
@@ -126,7 +135,7 @@ function createGalerryDom(obj) {
       <b>Downloads: ${downloads}</b>
     </p>
   </div>
-</div>`
+</div> </a>`
     )
     .join('');
 }
